0c3bcb95b0527e2e8b6b1720b6efce912554c19f
Moh Abdul Haq Aulia <abdul.haq.aulia@gmail.com>
2022-05-07 13:51:36 +0700

    add term and condition

---
+++ components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="footer">
+    <!-- Footer -->
+    <div class="bg-indigo-600 h-64 flex flex-wrap justify-center pt-10" id="contact-us">
+        <div class="container">
+            <div class="grid grid-rows-3 grid-flow-col gap-4 pt-3">
+                <div class="row-span-3 text-white">
+                    <img class="h-10" src="../assets/images/logo-negative.png" alt="Reji">
+                    <p>Reji POS</p>
+                    <p>Aplikasi kasir untuk semua</p>
+                    <img class="h-10"
+                        src="https://deksq7vjxbt7a.cloudfront.net/storefront/default_v2/images/android-eng-min.png"
+                        alt="">
+                </div>
+                <div class="row-span-3 text-white">
+                    <p>Tetap Terhubung</p>
+                    <p>Moh. Abdul Haq Aulia</p>
+                    <p><a href="mailto:abdul.haq.aulia@gmail.com">abdul.haq.aulia@gmail.com</a></p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- components/Landing.vue
+++ components/Landing.vue
@@ -10,78 +10,7 @@
           </svg>
 
           <div>
-            <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
-              <nav class="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-                <div class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                  <div class="flex items-center justify-between w-full md:w-auto">
-                    <a href="#">
-                      <span class="sr-only">Reji</span>
-                      <img class="h-8 w-auto sm:h-10" src="../assets/images/logo.png">
-                    </a>
-                    <div class="-mr-2 flex items-center md:hidden">
-                      <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-expanded="false" @click="menuShow = !menuShow">
-                        <span class="sr-only">Open main menu</span>
-                        <!-- Heroicon name: outline/menu -->
-                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                          <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-                <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                  <a href="#home" class="font-medium text-gray-500 hover:text-gray-900">Home</a>
-
-                  <a href="#pricing" class="font-medium text-gray-500 hover:text-gray-900">Langganan</a>
-
-                  <a href="#" class="font-medium text-gray-500 hover:text-gray-900" @click="toTnc()">Term & Condition</a>
-
-                  <a href="#" class="font-medium text-gray-500 hover:text-gray-900" @click="toPC()">Privacy Policy</a>
-                </div>
-              </nav>
-            </div>
-
-            <!--
-              Mobile menu, show/hide based on menu open state.
-
-              Entering: "duration-150 ease-out"
-                From: "opacity-0 scale-95"
-                To: "opacity-100 scale-100"
-              Leaving: "duration-100 ease-in"
-                From: "opacity-100 scale-100"
-                To: "opacity-0 scale-95"
-            -->
-            <div class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden" v-show="menuShow">
-              <div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                <div class="px-5 pt-4 flex items-center justify-between">
-                  <div>
-                    <img class="h-8 w-auto" src="../assets/images/logo.png" alt="">
-                  </div>
-                  <div class="-mr-2">
-                    <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" @click="menuShow = !menuShow">
-                      <span class="sr-only">Close main menu</span>
-                      <!-- Heroicon name: outline/x -->
-                      <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-                <div class="px-2 pt-2 pb-3 space-y-1">
-                  <a href="#home" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" @click="menuShow = false">Home</a>
-
-                  <a href="#pricing" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" @click="menuShow = false">Langganan</a>
-
-                  <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" @click="toTnc()">Term & Condition</a>
-
-                  <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" @click="toPC()">Privacy Policy</a>
-
-                  <!-- <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Marketplace</a>
-
-                  <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a> -->
-                </div>
-              </div>
-            </div>
+            <Menu/>
           </div>
 
           <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28" id="home">
@@ -157,47 +86,11 @@
         </div>
     </div>
 
-    <!-- Footer -->
-    <div class="bg-indigo-600 h-64 flex flex-wrap justify-center pt-10" id="contact-us">
-        <div class="container">
-            <div class="grid grid-rows-3 grid-flow-col gap-4 pt-3">
-                <div class="row-span-3 text-white">
-                    <img class="h-10" src="../assets/images/logo-negative.png" alt="Reji">
-                    <p>Reji POS</p>
-                    <p>Aplikasi kasir untuk semua</p>
-                    <img class="h-10"
-                        src="https://deksq7vjxbt7a.cloudfront.net/storefront/default_v2/images/android-eng-min.png"
-                        alt="">
-                </div>
-                <div class="row-span-3 text-white">
-                    <p>Tetap Terhubung</p>
-                    <p>Moh. Abdul Haq Aulia</p>
-                    <p><a href="mailto:abdul.haq.aulia@gmail.com">abdul.haq.aulia@gmail.com</a></p>
-                </div>
-
-            </div>
-        </div>
-    </div>
-
+    <Footer/>
   </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      menuShow: false
-    }
-  },
-  methods: {
-    toTnc() {
-      this.menuShow = false
-      this.$route.push('term-and-condition')
-    },
-    toPC() {
-      this.menuShow = false
-      this.$route.push('privacy-policy')
-    }
-  }
 
 }
 </script>
